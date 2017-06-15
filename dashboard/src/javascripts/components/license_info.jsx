29a1d36fd3a0c4c047cbea0c46fc00d9fd8650c1
Andrew Klaus <andrewklaus@gmail.com>
2017-06-15 11:58:57 -0600

    Adding license required to license_info page

--- dashboard/src/javascripts/components/license_info.jsx
+++ dashboard/src/javascripts/components/license_info.jsx
@@ -5,16 +5,11 @@ import { AppShape } from "../shapes";
 
 class LicenseInfo extends React.Component {
   render() {
-    switch (this.props.app.licenseStatus) {
-    case "HAS_LICENSE_SURFMARKET":
-    case "HAS_LICENSE_SP":
-      return this.renderHasLicense();
-    case "NO_LICENSE":
-      return this.renderNoLicense();
-    case "NOT_NEEDED":
-      return this.renderNoLicenseNeeded();
-    case "UNKNOWN":
-      return this.renderUnknownLicense();
+    switch (this.props.app.licenseRequired) {
+    case true:
+      return this.renderLicenseRequired();
+    case false:
+      return this.renderNoLicenseRequired();
     default:
       return null;
     }
@@ -28,61 +23,23 @@ class LicenseInfo extends React.Component {
     return classNames;
   }
 
-  renderHasLicense() {
-    const licenseStatus = this.props.app.licenseStatus === "HAS_LICENSE_SURFMARKET" ? I18n.t("license_info.has_license_surfmarket") : I18n.t("license_info.has_license_sp");
-    const license = this.props.app.license;
-    const licenseInfo = (license && license.endDate) ? I18n.t("license_info.valid", { date: I18n.strftime(new Date(license.endDate), "%-d %B %Y") }) : "";
+  renderLicenseRequired() {
+    const licenseRequired = I18n.t("license_info.license_needed");
+    const licenseDetails = this.props.app.licenseDetails;
     return (
       <div className={this.renderSplitClass("license yes")}>
         <i className="fa fa-file-text-o"></i>
-        <h2>{licenseStatus}</h2>
-        <p>{licenseInfo}</p>
+        <h2>{licenseRequired}</h2>
+        <p>{licenseDetails}</p>
       </div>
     );
   }
 
-  renderNoLicense() {
-    let link;
-    if (this.props.showLinks) {
-      link = (
-        <p>
-          <Link to={`/apps/${this.props.app.id}/license_info`}>{I18n.t("license_info.license_info")}</Link>
-        </p>
-      );
-    }
-    return (
-      <div className={this.renderSplitClass("license no")}>
-        <i className="fa fa-file-text-o"></i>
-        <h2>{I18n.t("license_info.no_license")}</h2>
-        {link}
-      </div>
-    );
-  }
-
-  renderNoLicenseNeeded() {
+  renderNoLicenseRequired() {
     return (
       <div className={this.renderSplitClass("license no-needed")}>
         <i className="fa fa-file-text-o"></i>
         <h2>{I18n.t("license_info.no_license_needed")}</h2>
-      </div>
-    );
-  }
-
-  renderUnknownLicense() {
-    let link;
-    if (this.props.showLinks) {
-      link = (
-        <p>
-          <Link to={`/apps/${this.props.app.id}/license_info`}>{I18n.t("license_info.license_unknown_info")}</Link>
-        </p>
-      );
-    }
-
-    return (
-      <div className={this.renderSplitClass("license unknown")}>
-        <i className="fa fa-file-text-o"></i>
-        <h2>{I18n.t("license_info.unknown_license")}</h2>
-        {link}
       </div>
     );
   }
