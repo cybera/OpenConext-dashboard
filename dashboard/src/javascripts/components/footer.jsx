c96534037ae87ec9e6e4eddeac49b843a800371e
Andrew Klaus <andrewklaus@gmail.com>
2018-05-29 22:38:19 +0000

    Remove ToS from footer

--- dashboard/src/javascripts/components/footer.jsx
+++ dashboard/src/javascripts/components/footer.jsx
@@ -7,7 +7,6 @@ class Footer extends React.Component {
       <div className="mod-footer">
         <ul>
           <li dangerouslySetInnerHTML={{ __html: I18n.t("footer.surfnet_html") }}></li>
-          <li dangerouslySetInnerHTML={{ __html: I18n.t("footer.terms_html") }}></li>
           <li dangerouslySetInnerHTML={{ __html: I18n.t("footer.contact_html") }}></li>
         </ul>
       </div>
